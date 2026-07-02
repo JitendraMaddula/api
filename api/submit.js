c1c677432720d9c90d02c501968e8cdfbfa9c62e
JitendraMaddula <82214202+JitendraMaddula@users.noreply.github.com>
2026-07-02 14:00:28 +0530

    Simplify success message in submit.js

--- api/submit.js
+++ api/submit.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
                 join_date = EXCLUDED.join_date,
                 address = EXCLUDED.address;
         `;
-        res.status(200).json({ message: "Student saved/updated successfully!" });
+        res.status(200).json({ message: "Success" });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
